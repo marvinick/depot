0ac4cd34f13098baaf992178c5d1f30931d43cfa
marvinick <marviluw@gmail.com>
2014-01-07 07:57:30 -0600

    ch8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Depot::Application.routes.draw do
+
+  get "store/index"
   resources :products
+
+  root 'store#index', as: 'store'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
