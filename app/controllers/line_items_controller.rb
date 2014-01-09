1e766a9073d24164abf1f8a20f09787dfd9fafc9
marvinick <marviluw@gmail.com>
2014-01-09 03:47:21 -0600

    starting ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
