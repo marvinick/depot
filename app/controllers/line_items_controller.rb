e7ef48338d59c390e61b5691e953ed5bf9609398
marvinick <marviluw@gmail.com>
2014-01-09 22:18:32 -0600

    ch 11 ajax, coffeescript

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.js
+        format.html { redirect_to store_url }
+        format.js   { @current_item = @line_item }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
